ae1371df62a57babbf8c864bc4919e850e796604
jessica <fuerte.jessica10@gmail.com>
2022-03-03 21:25:54 -0600

    fixed routing issue

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const db = require('../users/users-model.js');
-const secrets = require('../../config/secrets.js');
+const secrets = require('./../config/secret');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
